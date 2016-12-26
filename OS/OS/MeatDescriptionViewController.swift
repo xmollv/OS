beba79602a736971c2530cbb1de24ed289dc28e5
Xavi Moll <xmollv@gmail.com>
2016-12-26 15:38:34 +0100

    Created the Photo model. Transformed the JSON into a Photo object. Loaded the downloaded images.

--- OS/OS/MeatDescriptionViewController.swift
+++ OS/OS/MeatDescriptionViewController.swift
@@ -13,18 +13,42 @@ protocol NetworkManagerClient {
 }
 
 class MeatDescriptionViewController: UIViewController {
-
+    
+    @IBOutlet var firstImage: UIImageView!
+    @IBOutlet var secondImage: UIImageView!
+    @IBOutlet var thirdImage: UIImageView!
+    @IBOutlet var fourthImage: UIImageView!
+    
     var networkManager: NetworkManager!
     private let flickrQuery = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a2846448fcd4963259bd4e2f4613f5b2&text=meat&safe_search=1&content_type=1&media=photos&per_page=4&format=json&nojsoncallback=1"
+    private var photos = [Photo]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        downloadDataFromFlickr()
+    }
+    
+    private func downloadDataFromFlickr() {
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
-        networkManager.fetchDataFrom(serverUrl: flickrQuery) { result in
+        networkManager.fetchDataFrom(serverUrl: flickrQuery) { [weak weakSelf = self] result in
             switch result {
             case .success(let JSON):
-                dump(JSON)
+                if let jsonDict = JSON as? [String: Any] {
+                    if let photos = jsonDict["photos"] as? [String: Any] {
+                        if let photo = photos["photo"] as? [[String: Any]] {
+                            for elem in photo {
+                                if let photoModeled = Photo(dict: elem) {
+                                    weakSelf?.photos.append(photoModeled)
+                                }
+                            }
+                            DispatchQueue.main.async {
+                                weakSelf?.populateImages()
+                            }
+                        }
+                    }
+                } else {
+                    print("Error parsing the JSON to a Dictionary")
+                }
             case .failure(let error):
                 print(error.localizedDescription)
             }
@@ -33,11 +57,38 @@ class MeatDescriptionViewController: UIViewController {
             }
         }
     }
-
+    
+    private func populateImages() {
+        if !photos.isEmpty && photos.count == 4 {
+            firstImage.setImage(photo: photos[0])
+            secondImage.setImage(photo: photos[1])
+            thirdImage.setImage(photo: photos[2])
+            fourthImage.setImage(photo: photos[3])
+        } else {
+            print("Not enought photos. Try to download a few more. The query only downloads 4 images.")
+        }
+        
+    }
+    
 }
 
 extension MeatDescriptionViewController: NetworkManagerClient {
     func set(networkManager: NetworkManager) {
         self.networkManager = networkManager
+    }
+}
+
+extension UIImageView {
+    func setImage(photo: Photo) {
+        if let url = URL(string: photo.url) {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                guard let data = data, error == nil else { return }
+                
+                DispatchQueue.main.async() {
+                    self.image = UIImage(data: data)
+                }
+            }
+            task.resume()
+        }
     }
 }
