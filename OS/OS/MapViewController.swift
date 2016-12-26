708ffe5ed8a99f14aa6788216ab60644549cf42e
Xavi Moll <xmollv@gmail.com>
2016-12-26 16:13:40 +0100

    First approach at placing the map inside the app

--- OS/OS/MapViewController.swift
+++ OS/OS/MapViewController.swift
@@ -7,19 +7,15 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let camera = GMSCameraPosition.camera(withLatitude: 41.38, longitude: 2.16, zoom: 12.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
