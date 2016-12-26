9a53753ecb0c896a75ad4ba795909b4dff687203
Xavi Moll <xmollv@gmail.com>
2016-12-26 17:38:32 +0100

    You're placed on the map and everything arround you is shown. I've to change it to show only meat related places

--- OS/OS/AppDelegate.swift
+++ OS/OS/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyAFWIsUPMERnnPOXw2EXDvL4myGxaGeFaM")
+        GMSPlacesClient.provideAPIKey("AIzaSyDh19k2TEccbZiD2CbVpe2FyBldIOg4URA")
         
         let networkManager = NetworkManager()
         
--- OS/OS/MapViewController.swift
+++ OS/OS/MapViewController.swift
@@ -8,14 +8,121 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 class MapViewController: UIViewController {
-
+    
+    @IBOutlet var mapContainer: UIView!
+    
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var mapView: GMSMapView!
+    var placesClient: GMSPlacesClient!
+    var zoomLevel: Float = 15.0
+    
+    // A default location to use when location permission is not granted.
+    let defaultLocation = CLLocation(latitude: 41.38, longitude: 2.16)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let camera = GMSCameraPosition.camera(withLatitude: 41.38, longitude: 2.16, zoom: 12.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
+        
+        // Initialize the location manager.
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        placesClient = GMSPlacesClient.shared()
+        
+        // Create a map.
+        let camera = GMSCameraPosition.camera(withLatitude: defaultLocation.coordinate.latitude,
+                                              longitude: defaultLocation.coordinate.longitude,
+                                              zoom: zoomLevel)
+        mapView = GMSMapView.map(withFrame: mapContainer.bounds, camera: camera)
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        // Add the map to the view, hide it until we've got a location update.
+        mapContainer.addSubview(mapView)
+        mapView.isHidden = true
     }
+    
+    // Add markers for the places nearby the device.
+    func updateMarkers() {
+        mapView.clear()
+        
+        // Get nearby places and add markers to the map.
+        placesClient.currentPlace(callback: { (placeLikelihoods, error) -> Void in
+            if let error = error {
+                print("Current Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let likelihoodList = placeLikelihoods {
+                for likelihood in likelihoodList.likelihoods {
+                    let place = likelihood.place
+                    
+                    let marker = GMSMarker(position: place.coordinate)
+                    marker.title = place.name
+                    marker.snippet = place.formattedAddress
+                    marker.map = self.mapView
+                }
+            }
+        })
+    }
+    
+}
+
+// Delegates to handle events for the location manager.
+extension MapViewController: CLLocationManagerDelegate {
+    
+    // Handle incoming location events.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        print("Location: \(location)")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
+        
+        //This prevents the location to be updated all the time. We just want what we have arround
+        locationManager.stopUpdatingLocation()
+        
+        updateMarkers()
+    }
+    
+    // Handle authorization for the location manager.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location.
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        }
+    }
+    
+    // Handle location manager errors.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error: \(error)")
+    }
+    
 }
 
