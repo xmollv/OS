8c5c20f8113970b5cd12342eb15f1f98fbb9a604
Xavi Moll <xmollv@gmail.com>
2016-12-31 18:36:37 +0100

    Added the model for the Tweets and parsed the JSON correctly

--- OS/OS/TweetsViewController.swift
+++ OS/OS/TweetsViewController.swift
@@ -11,18 +11,36 @@ import UIKit
 class TweetsViewController: UIViewController {
     
     var networkManager: NetworkManager!
+    var tweets = [Tweet]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let headers = ["Authorization":"OAuth oauth_consumer_key=\"DC0sePOBbQ8bYdC8r4Smg\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1483204579\",oauth_nonce=\"2180899013\",oauth_version=\"1.0\",oauth_token=\"815244804485283842-EJ7qtrT0EVElmbkIAAksLrI2T4kdDYO\",oauth_signature=\"eSUusclOOHsA2WCElz9i2OIvAvQ%3D\""]
+        downloadRecentTweets()
         
-        networkManager.fetchDataFrom(serverUrl: "https://api.twitter.com/1.1/search/tweets.json?q=meat", headers: headers) { result in
+    }
+    
+    private func downloadRecentTweets() {
+        
+        let headers = ["Authorization":"OAuth oauth_consumer_key=\"DC0sePOBbQ8bYdC8r4Smg\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1483204579\",oauth_nonce=\"2180899013\",oauth_version=\"1.0\",oauth_token=\"815244804485283842-EJ7qtrT0EVElmbkIAAksLrI2T4kdDYO\",oauth_signature=\"eSUusclOOHsA2WCElz9i2OIvAvQ%3D\""]
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        networkManager.fetchDataFrom(serverUrl: "https://api.twitter.com/1.1/search/tweets.json?q=meat", headers: headers) { [weak weakSelf = self] result in
             switch result {
             case .success(let JSON):
-                dump(JSON)
+                if let jsonParsed = JSON as? [String: Any] {
+                    if let tweets = jsonParsed["statuses"] as? [[String:Any]] {
+                        for tweet in tweets {
+                            if let tweetObject = Tweet(dict: tweet) {
+                                weakSelf?.tweets.append(tweetObject)
+                            }
+                        }
+                    }
+                }
             case .failure(let error):
                 print(error.localizedDescription)
+            }
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
             }
         }
     }
