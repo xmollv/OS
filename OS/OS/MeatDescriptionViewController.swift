1ff3ec6d4ad57cd45dd56dd58a2cc7fc72eb7c64
Xavi Moll <xmollv@gmail.com>
2017-01-01 12:10:06 +0100

    Handled the view when the user displays the keyboard. It will move up/down based on the keyboard

--- OS/OS/MeatDescriptionViewController.swift
+++ OS/OS/MeatDescriptionViewController.swift
@@ -23,6 +23,8 @@ class MeatDescriptionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
         downloadDataFromFlickr()
     }
     
@@ -80,6 +82,25 @@ extension MeatDescriptionViewController: NetworkManagerClient {
 extension MeatDescriptionViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         self.view.endEditing(true)
+    }
+}
+
+// Handle the keyboard when appears/disappears: http://stackoverflow.com/a/31124676/5683397
+extension MeatDescriptionViewController {
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0 {
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
     }
 }
 
